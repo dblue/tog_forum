bec371bf335d459f00450331e346594d54524efc
Dan Blue <dblue@gmail.com>
2009-12-15 11:53:24 -0500

    Added migration for acts_as_voteable.

--- tog_forum_template.rb
+++ tog_forum_template.rb
@@ -1,6 +1,10 @@
+# Dependencies
 plugin 'white_list_model', :git => "git://github.com/imanel/white_list_model.git"
-plugin 'tog_forum', :git => "git://github.com/aspgems/tog_forum.git"
-plugin 'acts_as_voteable', :git => "git://github.com/jaggederest/acts_as_voteable.git"
+plugin 'acts_as_voteable', :git => "git://github.com/aspgems/acts_as_voteable.git"
+generate "acts_as_voteable_migration"
+
+plugin 'tog_forum', :git => "git://github.com/dblue/tog_forum.git"
 route "map.routes_from_plugin 'tog_forum'"
+
 generate "update_tog_migration"
 rake "db:migrate"
